83599d9a117891b7b9562015ce2c4d5b95d3b54e
Ven <currency@gmail.com>
2023-06-06 19:56:54 +0530

    added toggle component

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div>
+        <UContainer>
+            <h1>Toggle</h1>
+            <UToggle
+                on-icon="i-heroicons-check-20-solid"
+                off-icon="i-heroicons-x-mark-20-solid" />
+        </UContainer>
+    </div>
 </template>
