07018c3fce528679bb61fc25b926a97e066bd18a
Ven <CareTiger@users.noreply.github.com>
2023-06-18 13:10:14 -0500

    notifications

--- app.vue
+++ app.vue
@@ -1,9 +1,30 @@
 <template>
-    <div>
+    <div class="">
         <UContainer>
-            <legend class="text-base font-semibold leading-6 block">
-                Notifications
-            </legend>
+            <div class="min-h-screen grid place-items-center">
+                <div>
+                    <p class="text-2xl font-semibold leading-6 block mb-6">
+                        Notifications
+                    </p>
+                    <ul class="flex flex-row space-x-4">
+                        <li>
+                            <UButton
+                                label="Show success toast"
+                                @click="toast.add({ title: 'Hello world!' })" />
+                        </li>
+                        <li>
+                            <UButton
+                                color="red"
+                                label="Show error toast"
+                                @click="toast.add({ title: 'Hello world!' })" />
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </UContainer>
+        <UNotifications />
     </div>
 </template>
+<script setup>
+const toast = useToast();
+</script>
